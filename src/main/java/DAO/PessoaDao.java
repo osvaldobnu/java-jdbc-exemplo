832592914b56cd1e0d381d21f36984fdbc6eca47
osvaldobnu <osvaldobnu@gmail.com>
2023-11-12 18:52:57 -0300

    ...

--- src/main/java/DAO/PessoaDao.java
+++ src/main/java/DAO/PessoaDao.java
@@ -74,7 +74,7 @@ public class PessoaDao {
 		}
 	}
 	
-	public static void atualizaPessoa(int id, String email, String nome) {
+	public static void atualizaPessoa(int id, String email) {
 		Connection conexao = null;
 		PreparedStatement st = null;
 		
@@ -82,7 +82,7 @@ public class PessoaDao {
 			conexao = BancoDados.getConexao();
 			
 			st = conexao.prepareStatement("UPDATE pessoa"
-					+ "SET email = ?, nome = ?"
+					+ "SET email = ?"
 					+ " WHERE id = ?");
 			
 			st.setString(1, email);
